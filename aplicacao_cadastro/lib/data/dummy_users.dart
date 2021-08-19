5dc1891d0ff7e50ff18bdd34a268e9571dd40d06
Carlos <carlosvini.amaral@gmail.com>
2021-08-19 15:45:59 -0300

    Versão 0.2
    Lista de Users

---
+++ aplicacao_cadastro/lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:aplicacao_cadastro/Models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/07/13/15/57/woman-6463925_960_720.jpg',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/08/14/07/21/child-6544738_960_720.png',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guilherme@abcmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/08/14/07/21/child-6544738_960_720.png',
+  ),
+};
---
+++ aplicacao_cadastro/lib/views/user_list.dart
@@ -0,0 +1,17 @@
+import 'package:aplicacao_cadastro/data/dummy_users.dart';
+import 'package:flutter/material.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const users = {...DUMMY_USERS};
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+      ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (ctx, i) => Text(users.values.elementAt(i).name)),
+    );
+  }
+}
